4bc29c33d37c2bc043931b204fd20ea970064539
Ashley <ashleyclairetanton@icloud.com>
2026-03-04 17:13:30 +0200

    suffered & conquered
    UI implementation: aim circle moves with player, health bar moves but doesnt rotate (kinda) and health bar is affected by damage

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -21,6 +21,9 @@ public class PlayerController : MonoBehaviour
     private Vector3 lookInput;
     public float health = 100f;
 
+    [Header("UI")]
+    [SerializeField] private HealthBar healthBar;
+
     private void Start()
     {
         playerInput.actions["Movement"].performed += ctx => moveInput = ctx.ReadValue<Vector2>();
@@ -61,6 +64,8 @@ public class PlayerController : MonoBehaviour
 public void TakeDamage(float damage)
     {
         health -= damage;
+        healthBar.UpdateHealthBar(health);
+
         if (health <= 0)
         {
             Die();
